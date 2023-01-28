1dbcc43883f5efa3ac5da6dddfd5bed6336af277
Andres-Barragan-Salas <A01026567@itesm.mx>
2023-01-28 13:50:37 -0600

    Added dotenv + dependency

--- Component.js
+++ Component.js
@@ -1,6 +1,11 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export default class Component {
   constructor(params, children) {
     this.params = params;
     this.children = children;
+    this.envVar = process.env.TEST_VAR
   }
 }
